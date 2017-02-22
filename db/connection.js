0431ccb80077f3976c21ae66a64137780d39c3b6
Meng72 <matthewkeng@hotmail.com>
2017-02-22 14:32:10 -0600

    yet again

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const pg = require('pg');
 
 var config = {
   // database: 'solo'
-  user: oyhrtdssihsime,
-  database: d5s56frcplpjbg,
-  password: cae7e6841d2825fdef29185ffc59c819689d2b7b03033b66d1733edf0c4907de,
-  host: ec2-23-21-224-106.compute-1.amazonaws.com,
+  user: 'oyhrtdssihsime',
+  database: 'd5s56frcplpjbg',
+  password: 'cae7e6841d2825fdef29185ffc59c819689d2b7b03033b66d1733edf0c4907de',
+  host: 'ec2-23-21-224-106.compute-1.amazonaws.com',
   ssl: true,
   port: 5432,
   max:10,
