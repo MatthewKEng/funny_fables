33a90aae34c753c1e1c1840fc5f8111db3f9a0f3
Meng72 <matthewkeng@hotmail.com>
2017-07-06 18:05:25 -0500

    add to db

--- db/connection.js
+++ db/connection.js
@@ -9,6 +9,8 @@ var config = {
   host: 'ec2-23-21-224-106.compute-1.amazonaws.com',
   ssl: true,
   port: 5432,
+  uri: 'oyhrtdssihsime:cae7e6841d2825fdef29185ffc59c819689d2b7b03033b66d1733edf0c4907de@ec2-23-21-224-106.compute-1.amazonaws.com:5432/d5s56frcplpjbg',
+  cli: 'heroku pg:psql postgresql-metric-41411 --app funnyfables',
   max:10,
   idleTimeoutMillis: 300000,
 };
